101b63936dfdd28682f3ad86591d231769ab5799
sonthidejphoosae <53294397+sonthidejphoosae@users.noreply.github.com>
2019-07-26 09:57:32 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your web key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "HS6BUEIOT/"; //enter your appid
+		$KEY = "TRG1CjzgxhSShNh"; //enter your web key
+		$SECRET = "y4mz6ZYYTZRxbCf0RdQn4ES9j"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
